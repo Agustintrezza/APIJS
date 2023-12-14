458b5c1f9b9f615044cc4126faa6df2e31b14e16
Agustintrezza <agustin.trezza@gmail.com>
2023-12-13 23:38:00 -0300

    Segundo Commit

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
 
 const router = express.Router();
 const mongoose = require('mongoose');
@@ -16,7 +16,7 @@ const yogaRouter = require('./routes/yoga');
 
 const stripe = require('stripe')('sk_test_51MpcnREFbtwHkKNRH6otmFgmIxUQR8k0y7gQyJatb71LanFGlJjhJGgt3LdX2i437H32i1DyFe7SJNF6fMgR0KCk00Rm9Rkgqw');
 
-dotenv.config()
+dotenv.config();
 
 app.use(express.json());
 app.use(router);
@@ -25,12 +25,39 @@ app.use("/health", (req, res) => res.sendStatus(200));
 // Habilita CORS para todas las rutas
 app.use(cors());
 
-app.use('/recycling', RecyclingRouter);
-app.use('/yoga', yogaRouter);
-app.use('/cocktails', cocktailsRouter);
-app.use('/composers', composersRouter);
-app.use('/planets', planetsRouter);
-app.use('/users', usersRouter);
+// Define la variable de entorno BASE_URL
+const baseUrl = process.env.BASE_URL || "http://localhost:4242";
+
+app.use('/recycling', (req, res) => {
+    // Usa la variable BASE_URL al construir la ruta
+    const fullUrl = `${baseUrl}/recycling`;
+    RecyclingRouter(req, res, fullUrl);
+});
+
+app.use('/yoga', (req, res) => {
+    const fullUrl = `${baseUrl}/yoga`;
+    yogaRouter(req, res, fullUrl);
+});
+
+app.use('/cocktails', (req, res) => {
+    const fullUrl = `${baseUrl}/cocktails`;
+    cocktailsRouter(req, res, fullUrl);
+});
+
+app.use('/composers', (req, res) => {
+    const fullUrl = `${baseUrl}/composers`;
+    composersRouter(req, res, fullUrl);
+});
+
+app.use('/planets', (req, res) => {
+    const fullUrl = `${baseUrl}/planets`;
+    planetsRouter(req, res, fullUrl);
+});
+
+app.use('/users', (req, res) => {
+    const fullUrl = `${baseUrl}/users`;
+    usersRouter(req, res, fullUrl);
+});
 
 async function connectToMongo() {
     try {
@@ -39,12 +66,9 @@ async function connectToMongo() {
         app.listen(process.env.PORT, () => {
             console.log('Conectado a la base de datos. Servidor corriendo en el puerto http://localhost:' + process.env.PORT)
         })
-    } catch(error) {
+    } catch (error) {
         console.log('error', error)
     }
 }
-
-
-
 
 connectToMongo();
